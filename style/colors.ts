64c7a00515f58cabe4255f77b482abd9f2667478
Izzy Berzsenyi <iberzsenyi@yahoo.com>
2024-07-06 09:18:43 -0700

    Import colors

---
+++ style/colors.ts
@@ -0,0 +1,12 @@
+import { Yellowtail } from "next/font/google";
+
+const colors = {
+  CANVAS: '#FFF8EE',
+  BABY_PINK: '#FFE0E0',
+  SUN_YELLOW: '#FEE59E',
+  LIGHT_GREEN: '#FBF2DA',
+  PEACH: '#FFE9CC',
+  BLACK: '#000000'
+};
+
+export { colors };
